b26be1be905aeb6c5b8b7483cdb7c0e206b61ae4
moadij <moalstigsson@gmail.com>
2020-03-22 17:57:50 -0600

    first commit

---
+++ src/components/header-bar-brand.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar-brand">
+    <a
+      class="navbar-item"
+      href="https://github.com/moadij"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i class="fas fa-mask"></i>
+      <!-- <i class="fab js-logo fa-vuejs fa-2x" aria-hidden="true" /> -->
+    </a>
+    <div class="navbar-item nav-home">
+      <span class="tour">HEROS</span>
+      <span class="of">AND</span>
+      <span class="heroes">VILLAINS</span>
+    </div>
+    <button
+      class="link navbar-burger burger"
+      aria-label="menu"
+      aria-expanded="false"
+      data-target="navbarBasicExample"
+    >
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </button>
+  </div>
+</template>
---
+++ src/views/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content-container">
+    <div class="section content-title-group">
+      <h2 class="title">Here are some awesome Super Heros </h2>
+      <p>Get inspired</p>
+      <br />
+      <h3 class="title sub-title">Source Code</h3>
+      <p>You can find the source code for this webpage at the link below</p>
+      <p>
+        <a
+          href="https://github.com/moadij"
+          target="_blank"
+          >moadij GitHub</a
+        >
+      </p>
+      <br />
+    </div>
+  </div>
+</template>
